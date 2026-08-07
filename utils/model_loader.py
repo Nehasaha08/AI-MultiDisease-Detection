32ac0dd44b3dfb13ebaec6b35d2e2b4a6fc8c173
NEHA SAHA <sahaneha214@gmail.com>
2026-08-07 21:35:13 +0530

    Download models from Hugging Face and load them
    Refactored model loading to download models from Hugging Face hub.

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,18 +1,28 @@
+import os
+from huggingface_hub import hf_hub_download
 from tensorflow.keras.models import load_model
 
-# Load once when app starts
-image_classifier = load_model(
-    "models/imageprocess_model.h5"
-)
+REPO_ID = "nehasaha08/ai-multidisease-models"
 
-pneumonia_model = load_model(
-    "models/pneumonia_model.h5"
-)
+os.makedirs("models", exist_ok=True)
 
-brain_model = load_model(
-    "models/brain-tumor_model.h5"
-)
 
-breast_model = load_model(
-    "models/breast-cancer_model.h5"
-)
+def download_model(filename):
+    return hf_hub_download(
+        repo_id=REPO_ID,
+        filename=filename,
+        local_dir="models",
+        local_dir_use_symlinks=False
+    )
+
+
+image_path = download_model("imageprocess_model.h5")
+pneumonia_path = download_model("pneumonia_model.h5")
+brain_path = download_model("brain-tumor_model.h5")
+breast_path = download_model("breast-cancer_model.h5")
+
+
+image_classifier = load_model(image_path)
+pneumonia_model = load_model(pneumonia_path)
+brain_model = load_model(brain_path)
+breast_model = load_model(breast_path)
