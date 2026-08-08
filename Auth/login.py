25003acf8c73e85ad283c728ba4800badcbaed37
NEHA SAHA <sahaneha214@gmail.com>
2026-08-08 15:19:50 +0530

    Update login.py

--- Auth/login.py
+++ Auth/login.py
@@ -1,13 +1,19 @@
 import sqlite3
 from Auth.auth_utils import verify_password
 
-def login_user(username,password):
 
-    conn = sqlite3.connect(
-        "database/users.db"
-    )
+def login_user(username, password):
 
+    conn = sqlite3.connect("database/users.db")
     cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
+    """)
 
     cursor.execute(
         "SELECT password FROM users WHERE username=?",
@@ -19,10 +25,6 @@ def login_user(username,password):
     conn.close()
 
     if user:
-
-        return verify_password(
-            password,
-            user[0]
-        )
+        return verify_password(password, user[0])
 
     return False
