f22e5d72ea4e35662f6162a79cd73f7c84cf1158
NEHA SAHA <sahaneha214@gmail.com>
2026-08-08 15:20:20 +0530

    Update register.py

--- Auth/register.py
+++ Auth/register.py
@@ -1,35 +1,33 @@
 import sqlite3
 from Auth.auth_utils import hash_password
 
+
 def register_user(username, password):
 
-    conn = sqlite3.connect(
-        "database/users.db"
-    )
-
+    conn = sqlite3.connect("database/users.db")
     cursor = conn.cursor()
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password BLOB
-    )
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
     """)
 
-    hashed = hash_password(password)
-
     try:
+        hashed_password = hash_password(password)
+
         cursor.execute(
-            "INSERT INTO users(username,password) VALUES(?,?)",
-            (username, hashed)
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            (username, hashed_password)
         )
 
         conn.commit()
+        conn.close()
+
         return True
 
-    except:
-        return False
-
-    finally:
+    except sqlite3.IntegrityError:
         conn.close()
+        return False
